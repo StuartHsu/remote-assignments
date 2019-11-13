5617b32bcf764e7c1072ca78ce664d3064c6fc65
Stuart Hsu <yopahsu@gmail.com>
2019-11-13 15:52:51 +0800

    side-nav bar v1.2 modify issue: open & close sidenav then resize window can't disable toggle icon

--- week-2/Assignment-4/app.js
+++ week-2/Assignment-4/app.js
@@ -1,19 +1,62 @@
+/* ==============================
+---------- Selector ----------
+============================== */
+
+/* ---------- Change Text ---------- */
+const banner = document.querySelector('.banner h1');
+
+/* ---------- Side Navbar ---------- */
 const openButton = document.querySelector('.open');
 const sideNav = document.querySelector('.main-nav');
 const closeButton = document.querySelector('.close');
 
-openButton.addEventListener('click', () => {
-  sideNav.style.width = '50%';
-  openButton.style.display = 'none';
+/* ---------- Show More ---------- */
+const button = document.querySelector('.button');
+const secondContent = document.querySelector('.second');
+
+
+/* ==============================
+---------- Function ----------
+============================== */
+
+function openNav() {
+  sideNav.style.width = "50%";
+  openButton.style.display = "none";
+}
+
+function closeNav() {
+  sideNav.style.width = "";
+  openButton.style.display = "block";
+}
+
+function addContent() {
+  secondContent.style.display = "block";
+}
+
+
+/* ==============================
+---------- EventListener ----------
+============================== */
+
+/* ---------- Change Text ---------- */
+banner.addEventListener('click', () => {
+  banner.textContent = "Have a Good Time!";
 });
 
-closeButton.addEventListener('click', () => {
-  sideNav.style.width = '';
-  openButton.style.display = 'block';
-});
+/* ---------- Side Navbar ---------- */
+openButton.addEventListener('click', openNav);
+closeButton.addEventListener('click', closeNav);
 
 
-// 當展開 sidebarNav 時，若更動視窗則重新讀取網頁
+// 若視窗大小變動中，則收合 sidebarNav
 window.addEventListener('resize', () => {
-  window.location.reload();
+  if (window.innerWidth <= 800) {
+      closeNav();
+  } else if (window.innerWidth > 800) {
+    sideNav.style.width = "";
+    openButton.style.display = "none";
+  }
 });
+
+/* ---------- Show More ---------- */
+button.addEventListener('click', addContent);
