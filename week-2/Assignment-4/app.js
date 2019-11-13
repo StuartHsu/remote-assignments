7cc8c81106de12278d7e7ad52ca1699048e5a09c
Stuart Hsu <yopahsu@gmail.com>
2019-11-13 13:22:15 +0800

    side-navbar v1.1 done

--- week-2/Assignment-4/app.js
+++ week-2/Assignment-4/app.js
@@ -1,1 +1,19 @@
+const openButton = document.querySelector('.open');
+const sideNav = document.querySelector('.main-nav');
+const closeButton = document.querySelector('.close');
 
+openButton.addEventListener('click', () => {
+  sideNav.style.width = '50%';
+  openButton.style.display = 'none';
+});
+
+closeButton.addEventListener('click', () => {
+  sideNav.style.width = '';
+  openButton.style.display = 'block';
+});
+
+
+// 當展開 sidebarNav 時，若更動視窗則重新讀取網頁
+window.addEventListener('resize', () => {
+  window.location.reload();
+});
