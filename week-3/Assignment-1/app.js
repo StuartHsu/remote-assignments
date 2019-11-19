238a6fd50da6ae3b14511cf54dde4c9052a17793
Stuart Hsu <yopahsu@gmail.com>
2019-11-19 15:59:18 +0800

    finish assignment-3

--- week-3/Assignment-1/app.js
+++ week-3/Assignment-1/app.js
@@ -2,10 +2,16 @@ const express = require('express');
 
 const app = express();
 
+app.use(express.static('public')); // static file
+
+// Assignment-1: Your First Web Server
 app.get('/', (req, res) => {
   res.send("<h1>Hello, teacher! I'm Stuart, this is Assignment-1</h1>")
 });
+// Assignment-1: End
 
+
+// Assignment-2: Build Backend API for Front-End
 app.get('/getData', (req, res) => {
   var number = req.query.number;
   if (number > 0) {
@@ -20,6 +26,15 @@ app.get('/getData', (req, res) => {
     res.send("<h1>Wrong Parameter</h1>");
   }
 });
+// Assignment-2: End
+
+
+// Assignment-3: Connect to Backend API by AJAX
+app.get('/sum.html', (req, res) => {
+  res.redirect('/sum');
+});
+// Assignment-3: End
+
 
 app.listen(3000, () => {
   console.log('Server started on port 3000');
